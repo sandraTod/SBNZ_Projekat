ec57400ff87d52efdda2293c78ac4cd2b27e0ce3
sandra_t <sakica995@gmail.com>
2024-05-07 14:41:54 +0200

    Inicijalni projekti

---
+++ backend/kjar/src/test/java/com/sbnz/kjar/KjarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbnz.kjar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KjarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ backend/model/src/test/java/com/sbnz/model/ModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbnz.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
