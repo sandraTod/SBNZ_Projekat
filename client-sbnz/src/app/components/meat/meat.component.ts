bbfe090c8e8a71d856b4998bd12862210420b4b3
sandra_t <sakica995@gmail.com>
2024-06-04 18:38:21 +0200

    Html i TypeScript za pregled svih vrsta mesa

--- client-sbnz/src/app/components/meat/meat.component.ts
+++ client-sbnz/src/app/components/meat/meat.component.ts
@@ -1,4 +1,8 @@
+import { MatDialog } from '@angular/material/dialog';
+import { MeatService } from './../../services/meat.service';
 import { Component, OnInit } from '@angular/core';
+import { Meat } from 'src/app/model/meat';
+import { DeleteEntityComponent } from '../delete-entity/delete-entity.component';
 
 @Component({
   selector: 'app-meat',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MeatComponent implements OnInit {
 
-  constructor() { }
+  listOfMeat!: Meat[];
+
+  constructor(private meatService: MeatService, private dialog: MatDialog) { }
 
   ngOnInit(): void {
+
+    this.meatService.getAllMeat().subscribe(data =>{this.listOfMeat = data});
+  }
+
+
+  addMeat(){}
+
+
+  deletePopup(id:any){
+
+    var deletePopup = this.dialog.open(DeleteEntityComponent,{
+      width: '30%',
+      height: '200px',
+
+    });
+    deletePopup.afterClosed().subscribe(item =>{ 
+      if(item == true){
+
+        //this.wineService.deleteWine(id).subscribe()
+
+      }
+
+    });
+
   }
 
 }
