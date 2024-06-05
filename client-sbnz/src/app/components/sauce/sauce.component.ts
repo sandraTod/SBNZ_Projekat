74e5a02dd29b3f13e2d174c0a6c7050ba28e812d
sandra_t <sakica995@gmail.com>
2024-06-05 14:13:39 +0200

    TypeScript i Html za prikaz svih soseva

--- client-sbnz/src/app/components/sauce/sauce.component.ts
+++ client-sbnz/src/app/components/sauce/sauce.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Sauce } from 'src/app/model/sauce';
+import { SauceService } from 'src/app/services/sauce.service';
 
 @Component({
   selector: 'app-sauce',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SauceComponent implements OnInit {
 
-  constructor() { }
+  listOfSauce!: Sauce[];
+
+  constructor(private sauceService: SauceService) { }
 
   ngOnInit(): void {
+    this.sauceService.getAllSauce().subscribe(data => {this.listOfSauce = data});
   }
+
+  deletePopup(id: any){}
+
+  addSauce(){}
 
 }
