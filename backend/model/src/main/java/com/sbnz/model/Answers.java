3ae64a3ef04034bb4357f913ef03073a71c7c4d2
sandra_t <sakica995@gmail.com>
2024-07-06 22:28:12 +0200

    Klasa za odgovore

---
+++ backend/model/src/main/java/com/sbnz/model/Answers.java
@@ -0,0 +1,100 @@
+package com.sbnz.model;
+
+public class Answers {
+	
+	private String ethnicity = "unknown";
+	private String healthConscious = "unknown";
+	private String onTheCoast = "unknown";
+	private String autmnOrWinter = "unknown";
+	private String preferMeat = "unknown";
+	private String lactoseIntolerant = "unknown";
+	private String summer = "unknown";
+	private String foodAllergy = "unknown";
+	private String pizzaWithPineapple = "unknown";
+	private String savoryFood  = "unknown";
+	private String onDate = "unknown";
+	private String spicyFood = "unknown";
+	private String sweetenedIcedTea = "unknown";
+	
+	
+	public String getEthnicity() {
+		return ethnicity;
+	}
+	public void setEthnicity(String ethnicity) {
+		this.ethnicity = ethnicity;
+	}
+	public String getHealthConscious() {
+		return healthConscious;
+	}
+	public void setHealthConscious(String healthConscious) {
+		this.healthConscious = healthConscious;
+	}
+	public String getOnTheCoast() {
+		return onTheCoast;
+	}
+	public void setOnTheCoast(String onTheCoast) {
+		this.onTheCoast = onTheCoast;
+	}
+	public String getAutmnOrWinter() {
+		return autmnOrWinter;
+	}
+	public void setAutmnOrWinter(String autmnOrWinter) {
+		this.autmnOrWinter = autmnOrWinter;
+	}
+	public String getPreferMeat() {
+		return preferMeat;
+	}
+	public void setPreferMeat(String preferMeat) {
+		this.preferMeat = preferMeat;
+	}
+	public String getLactoseIntolerant() {
+		return lactoseIntolerant;
+	}
+	public void setLactoseIntolerant(String lactoseIntolerant) {
+		this.lactoseIntolerant = lactoseIntolerant;
+	}
+	public String getSummer() {
+		return summer;
+	}
+	public void setSummer(String summer) {
+		this.summer = summer;
+	}
+	public String getFoodAllergy() {
+		return foodAllergy;
+	}
+	public void setFoodAllergy(String foodAllergy) {
+		this.foodAllergy = foodAllergy;
+	}
+	public String getPizzaWithPineapple() {
+		return pizzaWithPineapple;
+	}
+	public void setPizzaWithPineapple(String pizzaWithPineapple) {
+		this.pizzaWithPineapple = pizzaWithPineapple;
+	}
+	public String getSavoryFood() {
+		return savoryFood;
+	}
+	public void setSavoryFood(String savoryFood) {
+		this.savoryFood = savoryFood;
+	}
+	public String getOnDate() {
+		return onDate;
+	}
+	public void setOnDate(String onDate) {
+		this.onDate = onDate;
+	}
+	public String getSpicyFood() {
+		return spicyFood;
+	}
+	public void setSpicyFood(String spicyFood) {
+		this.spicyFood = spicyFood;
+	}
+	public String getSweetenedIcedTea() {
+		return sweetenedIcedTea;
+	}
+	public void setSweetenedIcedTea(String sweetenedIcedTea) {
+		this.sweetenedIcedTea = sweetenedIcedTea;
+	}
+	
+	
+}
