f1d90656e837f992ff968d1ca8d5b769f8a98b4c
sandra_t <sakica995@gmail.com>
2026-01-04 12:33:08 +0100

    Uradjene izmenice na back-u

--- backend/service/src/main/java/com/sbnz/service/repository/WineRepository.java
+++ backend/service/src/main/java/com/sbnz/service/repository/WineRepository.java
@@ -1,9 +1,13 @@
 package com.sbnz.service.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sbnz.model.Wine;
 
 public interface WineRepository extends JpaRepository<Wine, Long> {
+	
+	Wine findByNameIgnoreCase(String name); 
 
 }
